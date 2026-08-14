ffbaa25acf163dc3211e011f67d05779d0a2edea
YOUR_NAME <YOUR_EMAIL@example.com>
2026-08-14 18:10:04 +0300

    feat(save): add ZoneSaveData class for zone persistence

---
+++ Assets/Scripts/Core/Save/ZoneSaveData.cs
@@ -0,0 +1,11 @@
+using System;
+
+[Serializable]
+public class ZoneSaveData
+{
+    public int gridX;
+    public int gridY;
+    public ZoneType zoneType;
+    public int density;
+    public int maxBuildings;
+}
